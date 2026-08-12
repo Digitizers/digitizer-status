e8346ce5e9ccc562467fc2c1bf2e958120cd6ee3
Ben Kalsky <benkalsky@gmail.com>
2026-08-12 15:31:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://www.digitizer.co.il/wp-content/uploads/2020/02/Asset-3.svg">
+    <title>Digitizer Status Page</title>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-N2RB6P2X');</script>
+    <!-- End Google Tag Manager -->
+    <style>
+        html, body {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            overflow: hidden;
+            background: #e5eff9FF;
+        }
+    </style>
+</head>
+<body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N2RB6P2X"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <iframe id="myIframe" src="https://digitizer.robotalp.com/" style="width: 0; height: 0; visibility: hidden; position: absolute; border: none;"></iframe>
+
+    <script>
+        setTimeout(() => {
+            let iframe = document.getElementById("myIframe");
+            iframe.style.width = "100%";
+            iframe.style.height = "100vh";
+            iframe.style.visibility = "visible";
+            iframe.style.position = "static";
+        }, 3000); // מחכה 3 שניות לפני ההצגה
+    </script>
+
+</body>
+</html>
